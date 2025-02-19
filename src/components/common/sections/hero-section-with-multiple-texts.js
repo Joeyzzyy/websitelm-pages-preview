f8e79cc68b238d0a02d1e09181fd7b41a5b72bb5
Yue Zhu <zhuyuejoey@gmail.com>
2025-02-19 13:33:36 +0800

    features added

--- src/components/common/sections/hero-section-with-multiple-texts.js
+++ src/components/common/sections/hero-section-with-multiple-texts.js
@@ -53,6 +53,43 @@ const HeroSectionWithMultipleTexts = ({ data, theme = 'normal' }) => {
                   </button>
                 )}
 
+                {/* Discord 按钮 */}
+                {data.topContent.enableDiscord && data.topContent.discordLink && (
+                  <a
+                    href={data.topContent.discordLink}
+                    target="_blank"
+                    className="group relative w-full md:w-auto inline-flex items-center justify-center px-12 py-4 text-lg rounded-full font-semibold 
+                    transition-all duration-300 
+                    bg-[#5865F2] text-white overflow-hidden
+                    hover:bg-[#4752C4] hover:scale-[1.02] hover:shadow-[0_0_20px_rgba(88,101,242,0.6)]
+                    active:scale-95
+                    border-2 border-white/30 hover:border-white
+                    before:content-[''] before:absolute before:w-12 before:h-full before:top-0 before:-left-10
+                    before:bg-white/20 before:blur-[2px] before:-skew-x-30
+                    before:transition-transform before:duration-700
+                    hover:before:translate-x-[400px]
+                    after:content-[''] after:absolute after:w-full after:h-full after:top-0 after:left-0
+                    after:bg-gradient-to-r after:from-transparent after:via-white/10 after:to-transparent
+                    after:opacity-0 hover:after:opacity-100 after:transition-opacity after:duration-500"
+                  >
+                    <div className="absolute top-0 left-0 w-full h-full bg-[#5865F2] opacity-50 blur-2xl transition-all duration-300 group-hover:opacity-70"></div>
+                    <div className="relative flex items-center will-change-transform">
+                      <svg 
+                        className="w-6 h-6 mr-2 transition-all duration-300 group-hover:rotate-12 will-change-transform" 
+                        viewBox="0 0 24 24" 
+                        fill="currentColor"
+                      >
+                        <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515a.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0a12.64 12.64 0 0 0-.617-1.25a.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057a19.9 19.9 0 0 0 5.993 3.03a.078.078 0 0 0 .084-.028a14.09 14.09 0 0 0 1.226-1.994a.076.076 0 0 0-.041-.106a13.107 13.107 0 0 1-1.872-.892a.077.077 0 0 1-.008-.128a10.2 10.2 0 0 0 .372-.292a.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127a12.299 12.299 0 0 1-1.873.892a.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028a19.839 19.839 0 0 0 6.002-3.03a.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.956-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.955-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.946 2.418-2.157 2.418z"/>
+                      </svg>
+                      <span className="relative overflow-hidden will-change-transform">
+                        Join Discord
+                        <span className="absolute -bottom-1 left-0 w-0 h-[2px] bg-white/80 transition-all duration-300 
+                          group-hover:w-full group-hover:shadow-[0_0_10px_rgba(255,255,255,0.8)]"></span>
+                      </span>
+                    </div>
+                  </a>
+                )}
+
                 {/* Product Hunt Widget */}
                 {data.topContent.enableProductHunt && data.topContent.productHuntId && (
                   <a 
